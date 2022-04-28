7c89e361707284c5b9b351c686c9d6f75599a139
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-29 00:43:14 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <Navbar collapseOnSelect expand="lg"   className='header-container bg-success' variant="dark">
+                <Container >
+                    <Navbar.Brand as={Link} to="/">
+                        <h4>Digital Camera Warehouse</h4>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav className='nav-style my-1'>
+                            <Nav.Link as={CustomLink} to='/login' className='mt-2 fw-bold'>Login</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/signup' className='mt-2 fw-bold'>Register</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
