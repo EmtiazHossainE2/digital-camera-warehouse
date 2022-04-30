3f858d3e01ded8694d272d8fc2cbe1e5bccfeb45
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-30 22:38:57 +0600

    stoke item form validation

--- src/pages/Account/StokeItem/StokeItem.js
+++ src/pages/Account/StokeItem/StokeItem.js
@@ -1,9 +1,41 @@
+import axios from 'axios';
 import React from 'react';
 import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
-import { FcCameraAddon } from 'react-icons/fc';
+import toast from 'react-hot-toast';
+import { ImBoxAdd } from 'react-icons/im';
 import './StokeItem.css'
 
 const StokeItem = () => {
+
+
+    //handleStoke 
+    const handleStoke = event => {
+        event.preventDefault()
+        console.log('clicked');
+        const cameraInfo = {
+            name: event.target.name.value,
+            description: event.target.description.value,
+            price: event.target.price.value,
+            quantity:event.target.quantity.value,
+            img: event.target.img.value,
+            supplier: event.target.supplier.value,
+            afPoints: event.target.afPoints.value,
+            modes: event.target.modes.value,
+            movieType: event.target.movieType.value,
+            brand: event.target.brand.value,
+            brandId: event.target.brandId.value,
+            sold: event.target.sold.value,
+            ratings: event.target.ratings.value
+        }
+        // console.log(cameraInfo);
+        if (cameraInfo.name === '' || cameraInfo.price === '' || cameraInfo.description === '' || cameraInfo.img === '' || cameraInfo.quantity === '' || cameraInfo.supplier === '' || cameraInfo.afPoints === '' || cameraInfo.modes === '' || cameraInfo.movieType === '' || cameraInfo.brand === '' || cameraInfo.brandId === '' || cameraInfo.sold === '' || cameraInfo.ratings === '' ) {
+            toast.error(`Please Give All Info `, { id: "info" });
+        }
+        
+    }
+
+
+
     return (
         <div className=' py-5  stoke-container'>
             <h2 className='text-center pt-3 '> Stoke Item From Here</h2>
@@ -11,71 +43,73 @@ const StokeItem = () => {
                 <hr />
             </div>
             <div className="container w-75 mx-auto py-5 stoke-form">
-                <Form noValidate >
-                    <Row className="mb-3 fst-italic fw-bold">
-                        <Form.Group className='mb-3' as={Col} md="6" controlId="validationCustomUsername">
-                            {/* <Form.Label>Camera Name</Form.Label> */}
-                            <InputGroup hasValidation>
-                                <Form.Control
-                                    type="text"
-                                    name='name'
-                                    placeholder="Camera Name"
-                                    aria-describedby="inputGroupPrepend"
-                                    required
-                                />
-                            </InputGroup>
+                <Form noValidate onSubmit={handleStoke} >
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="4" controlId="validationCustom01">
+                            <Form.Control
+                                required
+                                type="text"
+                                name='name'
+                                placeholder="Camera Name"
+                            />
                         </Form.Group>
-                        <Form.Group className='mb-3' as={Col} md="6" controlId="validationCustomUsername">
-                            {/* <Form.Label>AF Points</Form.Label> */}
-                            <InputGroup hasValidation>
-                                <Form.Control
-                                    type="text"
-                                    name='afPoints'
-                                    placeholder="AF Points"
-                                    aria-describedby="inputGroupPrepend"
-                                    required
-                                />
-                            </InputGroup>
+                        <Form.Group as={Col} md="4" controlId="validationCustom02">
+                            <Form.Control
+                                required
+                                type="number"
+                                name='price'
+                                placeholder="Price"
+                            />
                         </Form.Group>
-                        <Form.Group className='mb-3' as={Col} md="6" controlId="validationCustomUsername">
-                            {/* <Form.Label>Camera Name</Form.Label> */}
+                        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
                             <InputGroup hasValidation>
                                 <Form.Control
-                                    type="text"
-                                    name='name'
-                                    placeholder="Camera Name"
-                                    aria-describedby="inputGroupPrepend"
-                                    required
-                                />
-                            </InputGroup>
-                        </Form.Group>
-                        <Form.Group className='mb-3' as={Col} md="6" controlId="validationCustomUsername">
-                            {/* <Form.Label>AF Points</Form.Label> */}
-                            <InputGroup hasValidation>
-                                <Form.Control
-                                    type="text"
-                                    name='afPoints'
-                                    placeholder="AF Points"
-                                    aria-describedby="inputGroupPrepend"
-                                    required
+                                    type="number"
+                                    name='quantity'
+                                    placeholder="Quantity"
                                 />
                             </InputGroup>
                         </Form.Group>
                     </Row>
-                    <Row className="mb-3 mt-4 fst-italic">
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="3" controlId="validationCustom03">
+                            <Form.Control type="text" name='brand' placeholder="Brand" required />
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom03">
+                            <Form.Control type="number" name='sold' placeholder="Sold" required />
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom03">
+                            <Form.Control type="number" name='ratings' placeholder="Ratings" required />
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom03">
+                            <Form.Control type="number" name='brandId' placeholder="Brand Id " required />
+                        </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
                         <Form.Group as={Col} md="6" controlId="validationCustom03">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" rows={3} type="text" name='description' placeholder="Enter description" required />
+                            <Form.Control type="text" name='supplier' placeholder="Supplier Name" required />
                         </Form.Group>
-                        <Form.Group as={Col} md="6" controlId="validationCustom04">
-                            <Form.Label>Image</Form.Label>
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
                             <Form.Control type="text" name='img' placeholder="Image url" required />
-
                         </Form.Group>
                     </Row>
-                    <Button variant="warning" type="submit" className=' fs-5'>
-                        Submit <span className='ps-3 '><FcCameraAddon /></span>
-                    </Button>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
+                            <Form.Control as="textarea" rows={2} type="text" name='afPoints' placeholder="AF Points" required />
+                        </Form.Group>
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
+                            <Form.Control as="textarea" rows={2} type="text" name='movieType' placeholder="Movie Type" required />
+                        </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
+                            <Form.Control as="textarea" rows={3} type="text" name='modes' placeholder="Modes" required />
+                        </Form.Group>
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
+                            <Form.Control as="textarea" rows={3} type="text" name='description' placeholder="Description" required />
+                        </Form.Group>
+                    </Row>
+                    <Button className='bg-success fw-bold mt-3' type="submit">Stoke Now <span className='fs-5 ps-3 '><ImBoxAdd /></span></Button>
                 </Form>
             </div>
         </div>
