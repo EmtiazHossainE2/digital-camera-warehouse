b10a094c33131a75265189f3e8e48b713569369f
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-30 19:21:29 +0600

    manage item page basic desing

--- src/pages/Account/ManageInventories/ManageInventories.js
+++ src/pages/Account/ManageInventories/ManageInventories.js
@@ -23,22 +23,38 @@ const ManageInventories = () => {
     }
 
     return (
-        <div className='py-5 container'>
-            <h2 className='text-center '>Manage All Items : {products.length}</h2>
+        <div className='mt-5 py-5 '>
+            <h2 className='text-center '>Manage Items : {products.length}</h2>
             <div className='hr-style mx-auto  mb-3 '>
             </div>
-
-            <div>
-                <div className="row">
-                    {
-                        products.map((product, index) => <ManageItem
-                            key={product._id}
-                            index={index}
-                            product={product}
-                            handleDelete={handleDelete}
-                        ></ManageItem>)
-                    }
+            <div className='container py-3  text-light' style={{ backgroundColor: '#578295' }}>
+                <div className="row ">
+                    <div className="col-md-1  d-flex justify-content-center pb-1  align-items-center">
+                        <span className='pe-5'>List</span>
+                    </div>
+                    <div className="col-md-1 d-flex justify-content-center pb-1  align-items-center">
+                        <h5> Image</h5>
+                    </div>
+                    <div className="col-md-2 d-flex justify-content-center pb-1  align-items-center">
+                        <h5>Name</h5>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-center pb-1  align-items-center">
+                        <h5>Product Info</h5>
+                    </div>
+                    <div className="col-md-2 d-flex justify-content-center pb-1  align-items-center">
+                        <h5>Delete</h5>
+                    </div>
                 </div>
+            </div>
+            <div>
+                {
+                    products.map((product, index) => <ManageItem
+                        key={product._id}
+                        product={product}
+                        index={index}
+                        handleDelete={handleDelete}
+                    ></ManageItem>)
+                }
             </div>
         </div>
     );
--- src/pages/Account/ManageItem/ManageItem.js
+++ src/pages/Account/ManageItem/ManageItem.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import deleteIcon from '../../../images/delete.png'
 import './ManageItem.css'
 
 const ManageItem = ({ product, index, handleDelete }) => {
-    const { name,img,description,price,quantity,supplier , _id , sold} = product
+    const { name, img, description, price, quantity, supplier, _id, sold } = product
 
     // {name,description,price,quantity,img,supplier,afPoints , brand,brandId,modes,movieType,sold,ratings}
 
     return (
-        <div className='col-lg-4 col-md-6 rounded-1'>
-            <h3 className='text-center rounded-circle p-2'>{index}</h3>
-            <div className="cart mx-2 my-4 border border-1 ">
-                <div className='text-center'>
-                    <img className='product-img p-1 w-100' src={img} alt="" />
+        <div className='container'>
+            <div className="row border">
+                <div className="col-md-1  d-flex justify-content-center pb-1  align-items-center">
+                    <span className='pe-5'>{index + 1}</span>
                 </div>
-                <div className='p-3'>
-                    <h5 className=' text-capitalize'>{name}</h5>
-                    <p className='mb-0'>{description.slice(0, 100)}</p>
-                    <h6 className='mt-2 mb-0 fw-bolder'>Price : $ {price}</h6>
-                    <h6 className='mt-2 mb-0 fw-bolder'>Quantity : {quantity}</h6>
-                    <h6 className='mt-2 mb-0 fw-bolder'>Supplier : {supplier}</h6>
-                    <h6 className='mt-2 mb-0 fw-bolder'>Supplier : {sold}</h6>
-                    <button onClick={() => handleDelete(_id)} className='btn btn-danger'>Delete</button>
+                <div className="col-md-1 d-flex justify-content-center pb-1  align-items-center">
+                    <h5>  <img className=' table-img ' src={img} alt="" /></h5>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center pb-1  align-items-center">
+                    <h5>{name}</h5>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center pb-1  align-items-center">
+                    <h5>{name}</h5>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center pb-1  align-items-center">
+                    <div onClick={() => handleDelete(_id)} className='text-center' style={{cursor: 'pointer'}}>
+                        <img className='bg-danger w-50 p-2 rounded-circle' src={deleteIcon} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,3 +34,14 @@ const ManageItem = ({ product, index, handleDelete }) => {
 };
 
 export default ManageItem;
+
+{/* 
+<h3 className='text-center rounded-circle p-2'>{index}</h3>
+<img className='product-img p-1 w-100' src={img} alt="" />
+<h5 className=' text-capitalize'>{name}</h5>
+<p className='mb-0'>{description.slice(0, 100)}</p>
+<h6 className='mt-2 mb-0 fw-bolder'>Price : $ {price}</h6>
+<h6 className='mt-2 mb-0 fw-bolder'>Quantity : {quantity}</h6>
+<h6 className='mt-2 mb-0 fw-bolder'>Supplier : {supplier}</h6>
+<h6 className='mt-2 mb-0 fw-bolder'>Supplier : {sold}</h6>
+<button onClick={() => handleDelete(_id)} className='btn btn-danger'>Delete</button>                     */}
