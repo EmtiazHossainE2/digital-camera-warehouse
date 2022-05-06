20d7149d56cc8efe226a894590d7e4eab2e8409c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-06 16:43:12 +0600

    scroll to top

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter,  useLocation } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HelmetProvider>
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <Wrapper>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Wrapper>
     </BrowserRouter>
   </HelmetProvider>
 
